593444aab790c100260ce7a72e92872c863b9179
Federico Grandi <federicograndi@duck.com>
2022-10-12 21:58:35 +0200

    feat: add basic scraper function

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,75 @@
-console.log('Try npm run lint/fix!');
+import type {Axios} from 'axios';
+const axios: Axios = require('axios');
+import cheerio from 'cheerio';
 
-const longString =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
-
-const trailing = 'Semicolon';
-
-const why = 'am I tabbed?';
-
-export function doSomeStuff(
-  withThis: string,
-  andThat: string,
-  andThose: string[]
-) {
-  //function on one line
-  if (!andThose.length) {
-    return false;
-  }
-  console.log(withThis);
-  console.log(andThat);
-  console.dir(andThose);
-  return;
+interface Dependent {
+  repo: string;
+  stars: number;
+  forks: number;
 }
-// TODO: more examples
+
+interface ScrapeResult {
+  dependents: Dependent[];
+  nextURL: string | null;
+}
+
+export async function getDependents(repo: string, pageLimit = 500) {
+  const baseURL = `https://github.com/${repo}/network/dependents`;
+
+  const dependents: Dependent[] = [];
+  let nextURL: string | null = baseURL;
+
+  do {
+    pageLimit--;
+    const res: ScrapeResult = await scrapePage(nextURL);
+    dependents.push(...res.dependents);
+    nextURL = res.nextURL;
+  } while (pageLimit > 0 && nextURL);
+
+  console.log(dependents);
+  console.log(`${dependents.length} deps found`);
+}
+
+async function scrapePage(url: string): Promise<ScrapeResult> {
+  const res = await axios.get(url);
+
+  const $ = cheerio.load(res.data);
+
+  const dependents: Dependent[] = [];
+
+  $('#dependents > div.Box > div').each((i, e) => {
+    // The first element is the header row
+    if (i === 0) return;
+
+    const repo =
+      $(e).find('span > a:nth-child(1)').text().trim() +
+      '/' +
+      $(e).find('span > a:nth-child(2)').text().trim();
+
+    const stars = Number($(e).find('div > span:nth-child(1)').text().trim());
+    const forks = Number(
+      $(e)
+        .find('div > span:nth-child(2)')
+        .text()
+        .trim()
+        .split('\n')
+        .slice(-1)
+        .join('')
+        .trim()
+    );
+
+    dependents.push({
+      repo,
+      stars,
+      forks,
+    });
+  });
+
+  const nextURL =
+    $('#dependents > div.paginate-container > div > a').prop('href') || null;
+
+  return {
+    dependents,
+    nextURL,
+  };
+}
